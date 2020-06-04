830fd6365652b369994663c30f6c43f8ed166b77
Sabbagh <as@adn-realty.com>
2020-06-04 23:12:19 +0200

    add: bouton d'étage

--- src/components/Stage.vue
+++ src/components/Stage.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    Etage {{ this.nbr }}
-  </div>
+    <div class="text-center">
+        Etage {{ this.nbr }}
+        <div class="btn-list row justify-content-center mt-2" >
+            <button @click="addDestination(stage.value)" v-for="stage in stages" :key="stage.value" class="btn btn-info mr-1 btn-elevator">{{ stage.name }}</button>
+        </div>
+    </div>
 
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    nbr: String
-  }
-}
+    import {mapState} from 'vuex'
+
+    export default {
+        name: 'Stage',
+        props: {
+            nbr: Number
+        },
+        computed: {
+            ...mapState('stages', {
+                stages: state => state.stages
+            }),
+            ...mapState('elevator', {
+                waitingList: state => state.waitingList,
+            })
+        },
+        methods : {
+            addDestination : function (stage) {
+                this.$store.dispatch("elevator/addDestination", stage)
+            }
+        }
+    }
 </script>
