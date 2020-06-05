e75e42ba27fe6d8fc3e32db0d5106b6c804f5d65
Sabbagh <as@adn-realty.com>
2020-06-05 17:46:50 +0200

    add: style

---
+++ src/components/Elevator.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="elevator">
+        <img :src="require('../assets/images/elevator/elevator_' + this.mode + '.svg')" alt="">
+    </div>
+</template>
+<script>
+
+    import {mapState} from 'vuex'
+
+    export default {
+        name: 'Elevator',
+        computed: {
+            // VUEX
+                //Elevator
+            ...mapState('elevator', {
+                mode: state => state.mode,
+            }),
+        },
+    }
+</script>
+
